936d8bc0167d15814a2e2322fcf6266ac9847c87
SubhanaSaman <160390421+SubhanaSaman@users.noreply.github.com>
2024-10-04 11:32:07 +0500

    Add files via upload

---
+++ 4th-Milestone/index.ts
@@ -0,0 +1,23 @@
+
+document.getElementById('resume-form')?.addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent page refresh
+
+    // Get form values
+    const name = (document.getElementById('name') as HTMLInputElement).value;
+    const email = (document.getElementById('email') as HTMLInputElement).value;
+    const phone = (document.getElementById('phone') as HTMLInputElement).value;
+    const education = (document.getElementById('education') as HTMLInputElement).value;
+    const experince = (document.getElementById('experince') as HTMLInputElement).value;
+    const skills = (document.getElementById('skills') as HTMLInputElement).value;
+
+
+    // Display resume in the output div
+    (document.getElementById('resume-display') as HTMLElement).innerHTML = `
+        <p><strong>Name:</strong> ${name}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Phone:</strong> ${phone}</p>
+        <p><strong>Education:</strong> ${education}</p>
+        <p><strong>Experince:</strong> ${experince}</p>
+        <p><strong>Skills:</strong> ${skills}</p>`;
+});
+
